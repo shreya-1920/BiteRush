6f87df3e706d11794348345c8fe433b0e0927d47
shreya-1920 <shreyajain1902s@gmail.com>
2026-08-05 17:47:26 +0530

    server issue

--- backend/server.js
+++ backend/server.js
@@ -32,12 +32,14 @@ mongoose
 
 // ==================== Middleware ====================
 
+
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://bite-rush-umber.vercel.app/",
+      "https://bite-rush-umber.vercel.app",
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
 );
